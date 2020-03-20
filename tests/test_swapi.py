1264e3bc91155c0d52bf2481347a5bba745a4726
ynonp <ynonperek@gmail.com>
2020-03-20 21:13:55 +0200

    add pytest

---
+++ tests/test_swapi.py
@@ -0,0 +1,21 @@
+import requests
+import pytest
+
+def test_name():
+    with requests.Session() as s:
+        s.headers.update({
+            'Accept': 'application/json'
+        })
+        data = s.get('https://swapi.co/api/people/1').json()
+        assert data['name'] == 'Luke Skywalker'
+
+
+@pytest.fixture(scope='session')
+def req():
+    with requests.Session() as s:
+        s.headers.update({
+            'Accept': 'application/json'
+        })
+
+        yield s
+
