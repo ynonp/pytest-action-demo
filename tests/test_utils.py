e22f6cce7cd6f3cce85ace6cf91bb4c266025243
ynonp <ynonperek@gmail.com>
2020-05-17 17:25:22 +0300

    code cleanups

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -13,14 +13,11 @@ with codecs.open(str(datafile), encoding='utf8') as f:
 def test_twice_1(input, expected_output):
     assert utils.twice(input) == expected_output
 
-@pytest.mark.glob
 def test_thrice_1():
     assert utils.thrice(10) == 30, "thrice(10) != 30"
 
-@pytest.mark.hi
 def test_thrice_2():
     assert utils.thrice(0) == 0, "thrice(0) != 0"
-
 
 def test_thrice_3():
     assert utils.thrice(-5) == -15, "thrice(-5) != -15"
